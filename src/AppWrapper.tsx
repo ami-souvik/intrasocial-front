8f52a7c46165ab1596cd52e967a82657160efc1e
Souvik Dey <Ots1589@pidilite.net>
2024-09-24 16:55:39 +0530

    updt: modalize removed

--- src/AppWrapper.tsx
+++
@@ -1,9 +0,0 @@
-import ModalProvider from "./context/ModalContext";
-
-export default function AppWrapper({ children }: { children: any }) {
-  return (
-    <ModalProvider>
-      {children}
-    </ModalProvider>
-  )
-}
