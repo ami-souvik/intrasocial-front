da1def75231d228605f3b4c4bd4819facacc6385
Souvik Dey <Ots1589@pidilite.net>
2024-09-24 16:56:32 +0530

    updt: seperate screen for content creation

--- src/App.tsx
+++ src/App.tsx
@@ -8,10 +8,11 @@ import Profile from './views/profile/Profile'
 import { Feed } from '@/views/feed/Feed'
 import { Body, Left, Mid, Right } from '@/components/Body'
 import FeedTop from '@/views/feed/FeedTop'
+import ContentAdd from '@/views/content/ContentAdd'
 
 function App() {
   return (
-    <>
+    <div className='h-screen overflow-y-auto'>
       <FeedTop/>
       <Body>
         <Left/>
@@ -20,7 +21,7 @@ function App() {
         </Mid>
         <Right/>
       </Body>
-    </>
+    </div>
   )
 }
 
@@ -28,6 +29,10 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <Feed />,
+  },
+  {
+    path: "/content/create",
+    element: <ContentAdd />,
   },
   {
     path: "/content/:content",
