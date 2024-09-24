5abb41082a2bfe76df1d459bb34b0b4b25c4736d
Souvik Dey <Ots1589@pidilite.net>
2024-09-24 16:55:16 +0530

    updt: loading animation added

--- src/forms/ContentForm.tsx
+++ src/forms/ContentForm.tsx
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { useForm } from "react-hook-form";
 import { gql, useMutation } from "@apollo/client";
 import Tiptap from "./Tiptap";
@@ -16,6 +16,7 @@ const CREATE_CONTENT_MUTATION = gql`
       title: $title,
       body: $body) {
         content {
+          id
           title
           body
         }
@@ -32,6 +33,7 @@ const UPDATE_CONTENT_MUTATION = gql`
       title: $title,
       body: $body) {
         content {
+          id
           title
           body
         }
@@ -39,8 +41,8 @@ const UPDATE_CONTENT_MUTATION = gql`
   }
 `
 
-export default function ContentForm({ id, title, body, close, onSuccess }:
-  { id?: number, title?: string, body?: string, close?: () => void, onSuccess?: (v: ContentFormInputs) => void }) {
+export default function ContentForm({ data: { id, title, body }={}, close, onSuccess }:
+  { data?: { id?: number, title?: string, body?: string }, close?: () => void, onSuccess?: (v: ContentFormInputs) => void }) {
   const {
     control,
     register,
@@ -54,15 +56,29 @@ export default function ContentForm({ id, title, body, close, onSuccess }:
   const [createContent] = useMutation(CREATE_CONTENT_MUTATION);
   const [updateContent] = useMutation(UPDATE_CONTENT_MUTATION);
   const tiptap = useRef();
-  function onSubmit(data: ContentFormInputs) {
-      if(id) updateContent({ variables: { id, ...data } })
-        .then(() => { if(onSuccess) onSuccess(data) })
-      else createContent({ variables: { ...data } })
-        .then(() => { if(onSuccess) onSuccess(data) })
-      reset();
+  const [loading, setLoading] = useState(false);
+  async function onSubmit(data: ContentFormInputs) {
+    try {
+      setLoading(true)
+      let response;
+      if(id) {
+        response = await updateContent({ variables: { id, ...data } })
+        response = response.data.updateContent
+      }
+      else {
+        response = await createContent({ variables: { ...data } })
+        response = response.data.createContent
+      }
+      setLoading(false)
+      if(onSuccess) onSuccess(response.content)
       if(close) close();
+    }
+    catch(ex) {
+      setLoading(false)
+      console.log(ex);
+    }
   }
-  return <div>
+  return <div className="w-full">
     <div className="px-2 py-2 rounded-lg bg-neutral-950 overflow-x-hidden">
       <div className="space-y-2">
         <input className="p-1 w-full text-4xl font-bold border border-slate-600 bg-neutral-950 rounded-xl"
@@ -74,6 +90,9 @@ export default function ContentForm({ id, title, body, close, onSuccess }:
       </div>
       <div className="flex justify-end items-center mt-2">
         <button className="flex bg-teal-700 mx-2 space-x-2" onClick={handleSubmit(onSubmit)}>
+          {loading && <div className="w-[32px]">
+            <div className="w-[1px] text-slate-400 aspect-square rounded translate-x-[-16px] translate-y-[12px] animate-l21sm"/>
+          </div>}
           post
         </button>
         {close &&
