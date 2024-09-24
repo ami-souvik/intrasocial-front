dfb5a36586ac7a745468cf166fe09c0153232842
Souvik Dey <Ots1589@pidilite.net>
2024-09-24 16:56:10 +0530

    updt: created seperate screen for content addition

---
+++ src/views/content/ContentAdd.tsx
@@ -0,0 +1,9 @@
+import ContentForm from "@/forms/ContentForm";
+
+export default function ContentAdd() {
+    return <div className="my-4">
+        <ContentForm onSuccess={(data) => {
+            window.open(`/content/${data.id}`, '_self')
+        }} />
+    </div>
+}
